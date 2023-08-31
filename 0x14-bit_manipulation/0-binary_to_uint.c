64c085c140d9322d7b4769b36a52bb320f705f70
akubosilver <akubosilver@gmail.com>
2023-08-31 19:43:40 +0100

    Bit Task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@
 unsigned int binary_to_uint(const char *b) 
 {
     if (b == NULL)
-        return 0;
+        return (0);
 
     unsigned int result = 0;
     int i = 0;
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
     while (b[i] != '\0') 
     {
         if (b[i] != '0' && b[i] != '1')
-            return 0; // Invalid character found, return 0
+            return (0); // Invalid character found, return 0
         
         result = (result << 1) + (b[i] - '0'); // Convert and add the bit
         i++;
