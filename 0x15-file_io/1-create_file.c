4f739fa1a00d1652b206c59634bc3ec5ba9848b4
akubosilver <akubosilver@gmail.com>
2023-09-05 10:51:45 +0100

    Error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,14 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	wr = write(file, text_content, i);
-	if (wr == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		wr = write(file, text_content, i);
+		if (wr == -1)
+			return (-1);
+	}
 	close(file);
 	return (1);
 }
