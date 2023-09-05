79e61d91e44be56776e985e82d76551b509d3d00
akubosilver <akubosilver@gmail.com>
2023-09-05 10:59:18 +0100

    Error_2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
  * append_text_to_file - a function that appends
  * text at the end of a file
  * @filename: name of file to be opened for appending
- * @text_content
+ * @text_content: text to append
  *
  * Return: 1 on success and -1 on failure
  */
@@ -16,17 +16,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
-	if (file == -1)
+	if (file < 0)
 		return (-1);
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-	if (text_content == NULL)
-		return (-1);
-	wr = write(file, text_content, len);
-	if (wr == -1)
+	if (text_content)
 	{
-		close(file);
-		return (-1);
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(file, text_content, len);
+		if (wr < 0)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 	close(file);
 	return (1);
